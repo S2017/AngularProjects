b7125e5b5ca639676269e5e59be57d5b5bfde184
Siaha <crazysai0104@gmail.com>
2021-12-29 14:00:10 +0100

    Angular Tour of Heroes

---
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+// import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+  // hero : Hero = {
+  //   name: 'Windstorm',
+  //   id: 1
+  // } 
+
+  heroes: Hero[] = [];
+  constructor(private heroService: HeroService) { }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+    .subscribe(heroes =>  this.heroes = heroes);
+  }
+
+}
